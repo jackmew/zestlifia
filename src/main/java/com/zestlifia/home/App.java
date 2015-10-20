47bc918409404f70622b766d0ab9971853e49d81
Jackmew <wonderfuljack123@gmail.com>
2015-10-20 16:44:21 +0800

    zestlifia home

---
+++ src/main/java/com/zestlifia/home/App.java
@@ -0,0 +1,21 @@
+package com.zestlifia.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+    	SpringApplication.run(App.class, args);
+    }
+    
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Zestlifia";
+    }
+}
